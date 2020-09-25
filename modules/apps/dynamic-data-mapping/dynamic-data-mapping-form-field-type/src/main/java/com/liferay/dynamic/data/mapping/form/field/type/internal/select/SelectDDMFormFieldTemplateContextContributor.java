ce17f8f762efb166370c0071db166e79f15271f8
Roberto Díaz <roberto.diaz@liferay.com>
2020-09-25 11:47:56 +0200

    LPS-1214444 Allow set alphabetical order in SelectDDMFormField

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/select/SelectDDMFormFieldTemplateContextContributor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/select/SelectDDMFormFieldTemplateContextContributor.java
@@ -74,7 +74,8 @@ public class SelectDDMFormFieldTemplateContextContributor
 		parameters.put(
 			"options",
 			getOptions(
-				ddmFormFieldOptions, ddmFormFieldRenderingContext.getLocale(),
+				ddmFormField, ddmFormFieldOptions,
+				ddmFormFieldRenderingContext.getLocale(),
 				ddmFormFieldRenderingContext));
 
 		Locale displayLocale = LocaleThreadLocal.getThemeDisplayLocale();
@@ -137,11 +138,12 @@ public class SelectDDMFormFieldTemplateContextContributor
 		return ddmFormField.isMultiple();
 	}
 
-	protected List<Object> getOptions(
-		DDMFormFieldOptions ddmFormFieldOptions, Locale locale,
+	protected List<Map<String, String>> getOptions(
+		DDMFormField ddmFormField, DDMFormFieldOptions ddmFormFieldOptions,
+		Locale locale,
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
 
-		List<Object> options = new ArrayList<>();
+		List<Map<String, String>> options = new ArrayList<>();
 
 		for (String optionValue : ddmFormFieldOptions.getOptionsValues()) {
 			if (optionValue == null) {
@@ -160,6 +162,19 @@ public class SelectDDMFormFieldTemplateContextContributor
 				).put(
 					"value", optionValue
 				).build());
+		}
+
+		boolean alphabeticalOrder = GetterUtil.getBoolean(
+			ddmFormField.getProperty("alphabeticalOrder"));
+
+		if (alphabeticalOrder) {
+			options.sort(
+				(map1, map2) -> {
+					String label1 = map1.get("label");
+					String label2 = map2.get("label");
+
+					return label1.compareTo(label2);
+				});
 		}
 
 		return options;
