6a91aa77ce8f82cd04c6f942264560056b7858ba
Diego Nascimento <diego.nascimento@liferay.com>
2021-03-02 18:12:25 -0300

    LPS-124149 Migrate ShareFormModal to React
    We can have two possible ways to instantiate a ShareFormModal:
    
    * Using openShareFormModal function
    * Using directly ShareFormModalBody/Modal buttons React component
    
    The first one was necessary for the use case of view_form_instance.jsp file and the second one is used for calling the ModalContext redux with correct values.
    
    We're reusing the same ModalContext for avoiding have multiple ModalContexts around the application and it prevent duplicate errors

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/pages/FormBuilder.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/pages/FormBuilder.es.js
@@ -27,35 +27,50 @@ import React, {useCallback, useContext, useEffect, useRef} from 'react';
 import {FormInfo} from '../components/FormInfo.es';
 import {ManagementToolbar} from '../components/ManagementToolbar.es';
 import {MetalSidebarAdapter} from '../components/MetalSidebarAdapter.es';
+import ShareFormModalBody from '../components/ShareFormModal/ShareFormModalBody.es';
 import {TranslationManager} from '../components/TranslationManager.es';
 import {useAutoSave} from '../hooks/useAutoSave.es';
 import {useToast} from '../hooks/useToast.es';
 import fieldDelete from '../thunks/fieldDelete.es';
 import {createFormURL} from '../util/form.es';
+import {submitEmailContent} from '../util/submitEmailContent.es';
 
 export const FormBuilder = () => {
 	const {
+		autocompleteUserURL,
 		formInstanceId,
 		portletNamespace,
 		publishFormInstanceURL,
 		published,
 		redirectURL,
 		restrictedFormURL,
+		shareFormInstanceURL,
 		sharedFormURL,
 		showPublishAlert,
 		view,
 	} = useConfig();
+
 	const {
 		activePage,
 		editingLanguageId,
 		fieldSets,
 		focusedField,
+		localizedName,
 		pages,
 		rules,
 	} = useFormState();
 	const [{onClose}, modalDispatch] = useContext(ModalContext);
 
 	const dispatch = useForm();
+
+	const emailContent = useRef({
+		addresses: [],
+		message: Liferay.Util.sub(
+			Liferay.Language.get('please-fill-out-this-form-x'),
+			sharedFormURL
+		),
+		subject: localizedName[themeDisplay.getLanguageId()],
+	});
 
 	const {doSave, doSyncInput} = useAutoSave();
 
@@ -191,7 +206,80 @@ export const FormBuilder = () => {
 		[portletNamespace, subtmitForm]
 	);
 
-	const onShareClick = useCallback(() => {}, []);
+	const onShareClick = useCallback(async () => {
+		const settingsDDMForm = await Liferay.componentReady('settingsDDMForm');
+
+		const fields = settingsDDMForm.reactComponentRef.current.getFields();
+
+		const {value: requireAuthentication} = fields.find(
+			({fieldName}) => fieldName === 'requireAuthentication'
+		);
+
+		const url = createFormURL('', {
+			formInstanceId,
+			requireAuthentication,
+			restrictedFormURL,
+			sharedFormURL,
+		});
+
+		if (published) {
+			modalDispatch({
+				payload: {
+					body: (
+						<ShareFormModalBody
+							autocompleteUserURL={autocompleteUserURL}
+							emailContent={emailContent}
+							localizedName={localizedName}
+							url={url}
+						/>
+					),
+					footer: [
+						null,
+						null,
+						<ClayButton.Group key={1} spaced>
+							<ClayButton
+								displayType="secondary"
+								onClick={() => onClose()}
+							>
+								{Liferay.Language.get('cancel')}
+							</ClayButton>
+							<ClayButton
+								displayType="primary"
+								onClick={() => {
+									submitEmailContent({
+										addresses:
+											emailContent.current.addresses,
+										message: emailContent.current.message,
+										portletNamespace,
+										shareFormInstanceURL,
+										subject: emailContent.current.subject,
+									});
+
+									onClose();
+								}}
+							>
+								{Liferay.Language.get('done')}
+							</ClayButton>
+						</ClayButton.Group>,
+					],
+					header: Liferay.Language.get('share'),
+					size: 'lg',
+				},
+				type: 1,
+			});
+		}
+	}, [
+		autocompleteUserURL,
+		formInstanceId,
+		localizedName,
+		modalDispatch,
+		onClose,
+		portletNamespace,
+		published,
+		restrictedFormURL,
+		sharedFormURL,
+		shareFormInstanceURL,
+	]);
 
 	return (
 		<>
