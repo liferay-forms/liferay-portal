5acda94bb1b35c197c91f5a47f4a26eb91fd9e4a
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2021-05-07 12:12:31 -0300

    LPS-131828 Require Confirmation switcher shouldn't be displayed on Data Engine Clients

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/definition/JournalDataLayoutBuilderDefinition.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/definition/JournalDataLayoutBuilderDefinition.java
@@ -56,8 +56,8 @@ public class JournalDataLayoutBuilderDefinition
 	@Override
 	public String[] getUnimplementedProperties() {
 		return new String[] {
-			"allowGuestUsers", "fieldNamespace", "readOnly", "validation",
-			"visibilityExpression"
+			"allowGuestUsers", "fieldNamespace", "requireConfirmation",
+			"readOnly", "validation", "visibilityExpression"
 		};
 	}
 
