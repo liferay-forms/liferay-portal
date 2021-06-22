5043ab8f0925301ef1e52de25380bb68521a18d7
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-06-22 15:51:05 -0300

    LPS-134215 Separate ValidationDefault logic in a component and update unit test

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/types/src/main/resources/META-INF/resources/Validation/ValidationDefault.d.ts
@@ -0,0 +1,26 @@
+export default ValidationDefaultWrapper;
+declare function ValidationDefaultWrapper({
+	dataType: initialDataType,
+	defaultLanguageId,
+	editingLanguageId,
+	label,
+	name,
+	onChange,
+	readOnly,
+	spritemap,
+	validation,
+	value,
+	visible,
+}: {
+	dataType: any;
+	defaultLanguageId: any;
+	editingLanguageId: any;
+	label: any;
+	name: any;
+	onChange: any;
+	readOnly: any;
+	spritemap: any;
+	validation: any;
+	value?: {} | undefined;
+	visible: any;
+}): JSX.Element;
