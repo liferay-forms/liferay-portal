f5e670153972e47f10da6231624cd8091672d37c
IstvanD <istvan.dezsi@liferay.com>
2021-09-08 14:37:18 +0200

    LPS-136630 ddm-image should have it's own DDMFormFieldType and DDMFormFieldTypeSettings in order for the requiredDescription property to be read

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
@@ -1657,6 +1657,7 @@ public class DDMFormAdminDisplayContext {
 		List<DDMFormFieldType> ddmFormFieldTypes) {
 
 		List<String> ddmFormFieldTypesOutOfScope = Arrays.asList(
+			DDMFormFieldTypeConstants.DDM_IMAGE,
 			DDMFormFieldTypeConstants.GEOLOCATION,
 			JournalArticleDDMFormFieldTypeConstants.JOURNAL_ARTICLE,
 			LayoutDDMFormFieldTypeConstants.LINK_TO_LAYOUT);
