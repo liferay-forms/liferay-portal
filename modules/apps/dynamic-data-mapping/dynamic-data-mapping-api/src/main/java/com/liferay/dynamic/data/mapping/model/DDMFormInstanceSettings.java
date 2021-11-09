5ac204d034c30009333d7f58005529d213d6ac0b
Marcela Cunha <marcela.cunha@liferay.com>
2021-11-09 10:40:33 -0300

    LPS-141724 Update tip message

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/model/DDMFormInstanceSettings.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/model/DDMFormInstanceSettings.java
@@ -137,7 +137,7 @@ public interface DDMFormInstanceSettings {
 
 	@DDMFormField(
 		label = "%limit-to-one-submission-per-user",
-		tip = "%require-user-athentication-for-more-accurate-data",
+		tip = "%respondents-will-be-required-to-sign-in",
 		type = "checkbox"
 	)
 	public boolean limitToOneSubmissionPerUser();
