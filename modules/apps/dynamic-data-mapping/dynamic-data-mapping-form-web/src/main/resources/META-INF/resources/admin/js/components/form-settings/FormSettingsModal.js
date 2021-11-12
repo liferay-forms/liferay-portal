a02c77fb9a3a7a54f46062879e43cb9100b76fde
Aline Cantarelli <aline.cantarelli@liferay.com>
2021-11-11 22:03:39 -0300

    LPS-142259 Set alert visibility

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/components/form-settings/FormSettingsModal.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/components/form-settings/FormSettingsModal.js
@@ -52,7 +52,7 @@ const FormSettingsModal = ({
 	}, [dispatch, prevPagesRef, serializedSettingsContext]);
 
 	const {observer, onClose} = useModal({
-		onClose: () => {
+		onClose: async () => {
 			if (undoPagesRef.current) {
 				dispatch({
 					payload: prevPagesRef.current,
@@ -64,8 +64,29 @@ const FormSettingsModal = ({
 				});
 			}
 			else {
-				serializedSettingsContext.value = JSON.stringify({pages});
+				serializedSettingsContext.value = JSON.stringify({
+					pages,
+				});
 			}
+
+			const settingsDDMForm = await Liferay.componentReady(
+				'formSettingsAPI'
+			);
+
+			const showPartialResultsToRespondents = settingsDDMForm.reactComponentRef.current
+				.getFields()
+				.find(
+					({fieldName}) =>
+						fieldName === 'showPartialResultsToRespondents'
+				).value;
+
+			const showPartialResultsToRespondentsElement = document.querySelector(
+				'[id$="showPartialResultsToRespondents"]'
+			);
+
+			showPartialResultsToRespondentsElement.style.display = showPartialResultsToRespondents
+				? 'block'
+				: 'none';
 
 			undoPagesRef.current = true;
 			onCloseFormSettings();
