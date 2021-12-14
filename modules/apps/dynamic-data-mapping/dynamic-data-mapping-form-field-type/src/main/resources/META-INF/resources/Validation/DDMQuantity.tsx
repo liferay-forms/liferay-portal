b677c0360262df0b605465921d5959e6c43a59e1
Mateus Melo <mateus.melo@liferay.com>
2021-12-14 18:23:04 -0300

    LPS-143907 Creates DDMQuantity

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Validation/DDMQuantity.tsx
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ClayInput} from '@clayui/form';
+import React, {ChangeEvent, useState} from 'react';
+// @ts-ignore
+import { limitValue } from '../util/numericalOperations';
+
+const MAX_QUANTITY = 999;
+const MIN_QUANTITY = 1;
+
+const DDMQuantity: React.FC<IProps> = ({
+    label,
+    name,
+    onChange,
+    readOnly,
+    value: initialValue
+}) => {
+    const [value, setValue] = useState<string>(initialValue.toString());
+
+    const handleChange = () => {
+        value === '' ? '' : Math.abs(value);
+    }
+
+    return (
+    <>
+        <label>{label}</label>
+
+        <ClayInput
+            className="ddm-field-text"
+            disabled={readOnly}
+            max={MAX_QUANTITY}
+            min={MIN_QUANTITY}
+            name={name}
+            onBlur={onChange}
+            onChange={onChange}
+            type="number"
+            value={value}
+        />
+    </>)
+};
+
+export default DDMQuantity;
+
+interface IProps {
+    label: string,
+    name?: string,
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void,
+    readOnly?: boolean,
+    value: number
+}
