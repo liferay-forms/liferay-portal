f6fcf0dee5fa28741bf85ea6b96fc677fcaa8276
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-06-03 09:10:56 -0300

    LPS-155324 Keep consistency with ObjectEntryModelListener

--- modules/apps/object/object-api/src/main/java/com/liferay/object/system/model/listener/SystemObjectDefinitionMetadataModelListener.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/system/model/listener/SystemObjectDefinitionMetadataModelListener.java
@@ -174,11 +174,9 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 			"classPK", baseModel.getPrimaryKeyObj()
 		).put(
 			"model" + _modelClass.getSimpleName(),
-			_jsonFactory.createJSONObject(_jsonFactory.serialize(baseModel))
+			baseModel.getModelAttributes()
 		).put(
-			"modelDTO" + dtoConverterType,
-			_jsonFactory.createJSONObject(
-				_jsonFactory.serialize(_toDTO(baseModel, userId)))
+			"modelDTO" + dtoConverterType, _toDTO(baseModel, userId)
 		).put(
 			"objectActionTriggerKey", objectActionTriggerKey
 		).put(
@@ -188,8 +186,7 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 					return null;
 				}
 
-				return _jsonFactory.createJSONObject(
-					_jsonFactory.serialize(originalBaseModel));
+				return originalBaseModel.getModelAttributes();
 			}
 		).put(
 			"originalDTO" + dtoConverterType,
@@ -198,8 +195,7 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 					return null;
 				}
 
-				return _jsonFactory.createJSONObject(
-					_jsonFactory.serialize(_toDTO(originalBaseModel, userId)));
+				return _toDTO(originalBaseModel, userId);
 			}
 		);
 	}
@@ -219,7 +215,7 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 		return (Long)function.apply(baseModel);
 	}
 
-	private String _toDTO(T baseModel, long userId) throws PortalException {
+	private Object _toDTO(T baseModel, long userId) throws PortalException {
 		DTOConverter<T, ?> dtoConverter = _getDTOConverter();
 
 		if (dtoConverter == null) {
@@ -228,7 +224,7 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 					"No DTO converter found for " + _modelClass.getName());
 			}
 
-			return baseModel.toString();
+			return baseModel.getModelAttributes();
 		}
 
 		User user = _userLocalService.fetchUser(userId);
@@ -238,7 +234,7 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 				_log.warn("No user found with user ID " + userId);
 			}
 
-			return baseModel.toString();
+			return baseModel.getModelAttributes();
 		}
 
 		DefaultDTOConverterContext defaultDTOConverterContext =
@@ -247,8 +243,9 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 				user.getLocale(), null, user);
 
 		try {
-			return _jsonFactory.looseSerializeDeep(
-				dtoConverter.toDTO(defaultDTOConverterContext, baseModel));
+			return _jsonFactory.createJSONObject(
+				_jsonFactory.looseSerializeDeep(
+					dtoConverter.toDTO(defaultDTOConverterContext, baseModel)));
 		}
 		catch (Exception exception) {
 			if (_log.isDebugEnabled()) {
@@ -256,7 +253,7 @@ public class SystemObjectDefinitionMetadataModelListener<T extends BaseModel<T>>
 			}
 		}
 
-		return baseModel.toString();
+		return baseModel.getModelAttributes();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
