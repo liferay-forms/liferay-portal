52b7f5fd8627392ee7343b6df7c7e70b73dd2d76
Aline Cantarelli <aline.cantarelli@liferay.com>
2022-07-15 18:17:46 -0300

    LPS-154335 Handle zero and empty values typing

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/NumericInputMask/NumericInputMask.tsx
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/NumericInputMask/NumericInputMask.tsx
@@ -94,6 +94,9 @@ const NumericInputMask: React.FC<IProps> = ({
 	const [thousandsSeparator, setThousandsSeparator] = useState(
 		thousandsSeparatorInitial
 	);
+	const [currentDecimalPlaces, setCurrentDecimalPlaces] = useState(
+		decimalPlacesInitial
+	);
 	const [decimalPlaces, setDecimalPlaces] = useState(decimalPlacesInitial);
 	const [decimalSymbol, setDecimalSymbol] = useState(decimalSymbolInitial);
 	const [append, setAppend] = useState(appendInitial);
@@ -245,6 +248,7 @@ const NumericInputMask: React.FC<IProps> = ({
 								});
 
 								setDecimalPlaces(newValue);
+								setCurrentDecimalPlaces(newValue);
 								handleChange(
 									'decimalPlaces',
 									parseInt(newValue, 10)
@@ -256,19 +260,30 @@ const NumericInputMask: React.FC<IProps> = ({
 									? newValue.replace('-', '')
 									: newValue;
 
-								if (newValue <= MAX_DECIMAL_PLACES) {
-									newValue =
-										newValue === 0
-											? ''
-											: parseInt(newValue, 10);
+								if (newValue.length > 2 || newValue === '0') {
+									return;
+								}
+
+								if (newValue === '' || newValue === '0') {
+									setDecimalPlaces(DEFAULT_DECIMAL_PLACES);
+
+									handleChange(
+										'decimalPlaces',
+										DEFAULT_DECIMAL_PLACES
+									);
+								}
+								else if (newValue <= MAX_DECIMAL_PLACES) {
+									newValue = parseInt(newValue, 10);
 
 									setDecimalPlaces(newValue);
 
 									handleChange('decimalPlaces', newValue);
 								}
+
+								setCurrentDecimalPlaces(newValue);
 							}}
 							type="number"
-							value={decimalPlaces}
+							value={currentDecimalPlaces}
 						/>
 					</div>
 				</div>
