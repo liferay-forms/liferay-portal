5f83bc4489b2d303a33e0d19122b9babd2151763
hongvo2308 <hong.vo@codeenginestudio.com>
2022-08-24 11:45:50 +0700

    LPS-161353 Creating IsURLAcceptRootRelativeFunction for DDMExpressFunction

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/factory/IsURLAcceptRootRelativeFunctionFactory.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.evaluator.internal.function.factory;
+
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunctionFactory;
+import com.liferay.dynamic.data.mapping.form.evaluator.internal.function.IsURLAcceptRootRelativeFunction;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Hong Vo
+ */
+@Component(
+	property = "name=" + IsURLAcceptRootRelativeFunction.NAME,
+	service = DDMExpressionFunctionFactory.class
+)
+public class IsURLAcceptRootRelativeFunctionFactory
+	implements DDMExpressionFunctionFactory {
+
+	@Override
+	public DDMExpressionFunction create() {
+		return _IS_URL_ACCEPT_ROOT_RELATIVE_FUNCTION;
+	}
+
+	private static final IsURLAcceptRootRelativeFunction
+		_IS_URL_ACCEPT_ROOT_RELATIVE_FUNCTION =
+			new IsURLAcceptRootRelativeFunction();
+
+}
