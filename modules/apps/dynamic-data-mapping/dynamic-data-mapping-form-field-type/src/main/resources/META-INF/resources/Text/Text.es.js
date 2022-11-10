044ef368de929c38314274f1a4c4bd10edf9602d
valmir junior <valmiralvesjr2000@hotmail.com>
2022-11-10 17:42:04 -0300

    LPS-166350 Corrected the width to 25% and added the truncate an tooltip in text and select area

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Text/Text.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Text/Text.es.js
@@ -15,6 +15,7 @@
 import ClayAutocomplete from '@clayui/autocomplete';
 import ClayDropDown from '@clayui/drop-down';
 import {ClayInput} from '@clayui/form';
+import {ClayTooltipProvider} from '@clayui/tooltip';
 import {usePrevious} from '@liferay/frontend-js-react-web';
 import {normalizeFieldName} from 'data-engine-js-components-web';
 import {sub} from 'frontend-js-web';
@@ -143,42 +144,49 @@ const Text = ({
 
 	return (
 		<>
-			<ClayInput
-				className="ddm-field-text"
-				dir={Liferay.Language.direction[editingLanguageId]}
-				disabled={disabled}
-				id={id}
-				lang={editingLanguageId}
-				maxLength={showCounter ? '' : maxLength}
-				name={name}
-				onBlur={(event) => {
-					if (normalizeField) {
-						onBlur({target: {value: initialValue}});
-					}
-					else {
-						onBlur(event);
-					}
-				}}
-				onChange={(event) => {
-					const {value} = event.target;
+			<ClayTooltipProvider autoAlign>
+				<div data-tooltip-align="top" title={value}>
+					<ClayInput
+						className="ddm-field-text"
+						dir={Liferay.Language.direction[editingLanguageId]}
+						disabled={disabled}
+						id={id}
+						lang={editingLanguageId}
+						maxLength={showCounter ? '' : maxLength}
+						name={name}
+						onBlur={(event) => {
+							if (normalizeField) {
+								onBlur({target: {value: initialValue}});
+							}
+							else {
+								onBlur(event);
+							}
+						}}
+						onChange={(event) => {
+							const {value} = event.target;
 
-					if (normalizeField) {
-						event.target.value = normalizeFieldName(value);
-					}
-					else if (invalidCharacters) {
-						const regex = new RegExp(invalidCharacters, 'g');
+							if (normalizeField) {
+								event.target.value = normalizeFieldName(value);
+							}
+							else if (invalidCharacters) {
+								const regex = new RegExp(
+									invalidCharacters,
+									'g'
+								);
 
-						event.target.value = value.replace(regex, '');
-					}
-					setValue(event.target.value);
-					onChange(event);
-				}}
-				onFocus={onFocus}
-				placeholder={placeholder}
-				ref={inputRef}
-				type="text"
-				value={value}
-			/>
+								event.target.value = value.replace(regex, '');
+							}
+							setValue(event.target.value);
+							onChange(event);
+						}}
+						onFocus={onFocus}
+						placeholder={placeholder}
+						ref={inputRef}
+						type="text"
+						value={value}
+					/>
+				</div>
+			</ClayTooltipProvider>
 
 			<CounterContainer
 				counter={value?.length}
@@ -215,24 +223,28 @@ const Textarea = ({
 
 	return (
 		<>
-			<textarea
-				className="ddm-field-text form-control"
-				dir={Liferay.Language.direction[editingLanguageId]}
-				disabled={disabled}
-				id={id}
-				lang={editingLanguageId}
-				name={name}
-				onBlur={onBlur}
-				onChange={(event) => {
-					setValue(event.target.value);
-					onChange(event);
-				}}
-				onFocus={onFocus}
-				placeholder={placeholder}
-				style={disabled ? {resize: 'none'} : null}
-				type="text"
-				value={value}
-			/>
+			<ClayTooltipProvider autoAlign>
+				<div data-tooltip-align="top" title={value}>
+					<textarea
+						className="ddm-field-text form-control"
+						dir={Liferay.Language.direction[editingLanguageId]}
+						disabled={disabled}
+						id={id}
+						lang={editingLanguageId}
+						name={name}
+						onBlur={onBlur}
+						onChange={(event) => {
+							setValue(event.target.value);
+							onChange(event);
+						}}
+						onFocus={onFocus}
+						placeholder={placeholder}
+						style={disabled ? {resize: 'none'} : null}
+						type="text"
+						value={value}
+					/>
+				</div>
+			</ClayTooltipProvider>
 
 			<CounterContainer
 				counter={value?.length}
