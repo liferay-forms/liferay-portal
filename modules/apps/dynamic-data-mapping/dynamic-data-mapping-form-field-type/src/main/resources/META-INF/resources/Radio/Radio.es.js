0c2831a9f30a32519ec878c71c713e01bddd7f3a
Daniel Couso <daniel.couso@liferay.com>
2023-07-21 16:25:00 +0200

    LPS-191387 Avoid comparing by presence because value 0 is taken as not valid value when it should be

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Radio/Radio.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Radio/Radio.es.js
@@ -55,7 +55,11 @@ const Radio = ({
 	}, [predefinedValue]);
 
 	const [currentValue, setCurrentValue] = useSyncValue(
-		initialValue ? initialValue : predefinedValueMemo,
+		initialValue !== undefined &&
+			initialValue !== null &&
+			initialValue !== ''
+			? initialValue.toString()
+			: predefinedValueMemo,
 		true,
 		editingLanguageId
 	);
