d0840b7feea05709ff4dbce56bf16b27778f2fc6
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2024-09-24 00:01:52 -0300

    LPD-37074 Create test to assert functioning of data provider on virtual instance with include and override property

--- modules/test/playwright/tests/dynamic-data-mapping-form-web/formSubmission.spec.ts
+++ modules/test/playwright/tests/dynamic-data-mapping-form-web/formSubmission.spec.ts
@@ -8,11 +8,22 @@ import path from 'path';
 
 import {formsPagesTest} from '../../fixtures/formsPagesTest';
 import {loginTest} from '../../fixtures/loginTest';
+import {virtualInstancesPagesTest} from '../../fixtures/virtualInstancesPagesTest';
+import {liferayConfig} from '../../liferay.config';
+import {FormBuilderPage} from '../../pages/dynamic-data-mapping-form-web/FormBuilderPage';
+import {FormsPage} from '../../pages/dynamic-data-mapping-form-web/FormsPage';
+import performLogin, {performLogout} from '../../utils/performLogin';
 import {deleteItems} from './utils/deleteItems';
 
-export const test = mergeTests(loginTest(), formsPagesTest);
+export const test = mergeTests(
+	loginTest(),
+	formsPagesTest,
+	virtualInstancesPagesTest
+);
 
 let hasDataProvider: boolean = false;
+
+const DEFAULT_VIRTUAL_INSTANCE_NAME = 'www.able.com';
 
 test.afterEach(async ({formsPage, page}) => {
 	await formsPage.goTo();
@@ -31,6 +42,77 @@ test.afterEach(async ({formsPage, page}) => {
 });
 
 test.describe('Manage forms through submission page', () => {
+	test('assert that data provider works on virtual instance', async ({
+		browser,
+		virtualInstancesPage,
+	}) => {
+		hasDataProvider = true;
+
+		await virtualInstancesPage.addNewVirtualInstance(
+			DEFAULT_VIRTUAL_INSTANCE_NAME
+		);
+
+		const defaultBaseUrl = liferayConfig.environment.baseUrl;
+
+		liferayConfig.environment.baseUrl = `http://${DEFAULT_VIRTUAL_INSTANCE_NAME}:8080`;
+
+		const newPage = await browser.newPage({
+			baseURL: `http://${DEFAULT_VIRTUAL_INSTANCE_NAME}:8080`,
+		});
+
+		await performLogin(
+			newPage,
+			'test',
+			'?p_p_id=com_liferay_login_web_portlet_LoginPortlet&' +
+				'p_p_state=maximized',
+			`@${DEFAULT_VIRTUAL_INSTANCE_NAME}.com`
+		);
+
+		const newFormsPage = new FormsPage(newPage);
+
+		await newFormsPage.goTo();
+
+		await newFormsPage.importForm(
+			path.join(
+				__dirname,
+				'dependencies',
+				'form-with-select-field-data-provider.portlet.lar'
+			)
+		);
+
+		await newFormsPage.openForm(
+			'Form with data provider on virtual instance'
+		);
+
+		const formBuilderPage = new FormBuilderPage(newPage);
+
+		const pagePromise = newPage.waitForEvent('popup');
+
+		await formBuilderPage.openFormSubmission();
+
+		const formPreviewPage = await pagePromise;
+
+		await formPreviewPage
+			.getByLabel('Select from List', {exact: true})
+			.click();
+
+		await expect(
+			formPreviewPage.getByRole('option', {name: 'Algeria'})
+		).toBeVisible();
+
+		await formPreviewPage.close();
+
+		await performLogout(newPage);
+
+		await newPage.close();
+
+		liferayConfig.environment.baseUrl = defaultBaseUrl;
+
+		await virtualInstancesPage.deleteVirtualInstance(
+			DEFAULT_VIRTUAL_INSTANCE_NAME
+		);
+	});
+
 	test('can submit manual entry while using data provider autofill rule', async ({
 		context,
 		formBuilderPage,
